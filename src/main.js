e3823903037b3a5a2717c45ef25203bb3a6d20e7
Danny <ricoterox@gmail.com>
2016-12-25 18:48:52 -0300

    Se agrega metodo load

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 /* @flow */
 class Localizator {
+  constructor () {
+    this.source = '';
+  }
+
+  load (source) {
+    if (typeof source !== 'string') {
+      throw new Error('must be a string resouce');
+    }
+    this.source = source;
+  }
 }
 
 export default new Localizator();
--- test/main.test.js
+++ test/main.test.js
@@ -4,9 +4,25 @@ import abjson from '../src/main';
 
 tape('Localizator Json Test\'s', (t) => {
   const localizator = abjson;
-  t.test('should be exist', (q) => {
+  t.test('Should be exist', (q) => {
     q.equal(localizator, abjson);
     q.end();
   });
-});
 
+  t.test('Should has a load method', (q) => {
+    q.equal(typeof localizator.load, 'function');
+    q.end();
+  });
+
+  t.test('Should only accept 1 string parameter', (q) => {
+    q.throws(() => localizator.load(1), Error);
+    q.end();
+  });
+
+  t.test('Should paramater equal to source', (q) => {
+    const source = 'algo.json';
+    localizator.load(source);
+    q.equal(source, localizator.source);
+    q.end();
+  });
+});
